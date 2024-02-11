33fcfada7e25ba3fc679e399e49da1f9610c6ede
Anderson Vieira <asvieira.br@gmail.com>
2024-02-11 02:09:05 -0300

    feat(company): create integration between application and infrastructure layers

--- src/application/company/services/read-companies.service.ts
+++ src/application/company/services/read-companies.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 import { Company } from '../../../domain/entities/company';
 import { IReadCompaniesUseCase } from '../../../domain/usecases/read-companies.interface';
+import { CompanyDbRepository } from '../../../infrastructure/access/repositories/company/company-db.repository';
 
 @Injectable()
 export class ReadCompaniesService implements IReadCompaniesUseCase {
+  constructor(private readonly _companyDbRepository: CompanyDbRepository) {}
   async readAll(): Promise<Company[]> {
     return null;
   }
--- src/application/modules/company.module.ts
+++ src/application/modules/company.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { CompanyDbRepository } from '../../infrastructure/access/repositories/company/company-db.repository';
+import { CompanyModel } from '../../infrastructure/access/repositories/company/models/company.model';
 import { ReadCompaniesService } from '../company/services/read-companies.service';
 
 @Module({
-  providers: [ReadCompaniesService],
+  providers: [ReadCompaniesService, CompanyDbRepository],
+  imports: [TypeOrmModule.forFeature([CompanyModel])],
 })
 export class CompanyModule {}

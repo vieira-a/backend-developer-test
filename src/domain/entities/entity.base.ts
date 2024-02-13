dd8d8bee1d2d886445c1944b2b3c1974109eda5a
Anderson Vieira <asvieira.br@gmail.com>
2024-02-13 13:35:24 -0300

    refactor(core): set EntityBase fields createdAt and updatedAt as optionals

--- src/domain/entities/entity.base.ts
+++ src/domain/entities/entity.base.ts
@@ -1,9 +1,9 @@
 export abstract class EntityBase {
   public id?: string;
 
-  public createdAt: Date;
+  public createdAt?: Date;
 
-  public updatedAt: Date;
+  public updatedAt?: Date;
 
   protected constructor() {}
 }

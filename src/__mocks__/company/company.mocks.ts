1d8b36be635c8e997822f6c7e1861ef3c48bb4fb
Anderson Vieira <asvieira.br@gmail.com>
2024-02-13 11:42:46 -0300

    refactor(company): create OneToMany relationship between CompanyModel and JobModel

--- src/__mocks__/company/company.mocks.ts
+++ src/__mocks__/company/company.mocks.ts
@@ -4,12 +4,14 @@ export const companiesMock: CompanyModel[] = [
   {
     id: 'e76c5b34-230f-48d3-9ae5-3cb67f141903',
     name: 'Contoso',
+    jobs: [],
     createdAt: new Date(),
     updatedAt: new Date(),
   },
   {
     id: '57836e2d-5638-46c5-a847-7082d99bf3f4',
     name: 'Acme',
+    jobs: [],
     createdAt: new Date(),
     updatedAt: new Date(),
   },
--- src/infrastructure/access/repositories/company/models/company.model.ts
+++ src/infrastructure/access/repositories/company/models/company.model.ts
@@ -1,11 +1,15 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 
 import { BaseModel } from '../../../models';
+import { JobModel } from '../../job/models';
 
 @Entity('companies')
 export class CompanyModel extends BaseModel {
   @Column({ name: 'name', nullable: false, unique: true })
   public name: string;
+
+  @OneToMany(() => JobModel, (job) => job.company)
+  public jobs: JobModel[];
 
   protected constructor() {
     super();

b7e6f5a7b68f16c687facd77d3043616b8299c13
Anderson Vieira <asvieira.br@gmail.com>
2024-02-14 12:30:26 -0300

    feat(job): create domain layer with usecase

--- src/domain/usecases/job/index.ts
+++ src/domain/usecases/job/index.ts
@@ -1,3 +1,4 @@
 export * from './create-job-draft.interface';
 export * from './update-job-draft.interface';
 export * from './read-job-draft-by-id.interface';
+export * from './delete-job-draft.interface';

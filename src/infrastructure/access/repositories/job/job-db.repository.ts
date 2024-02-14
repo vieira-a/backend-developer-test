bdc5c4a773f3745e977ae75e3117fffec2deae30
Anderson Vieira <asvieira.br@gmail.com>
2024-02-13 23:09:27 -0300

    feat(job): implements application layer usecase in the infrastructure layer

--- src/infrastructure/access/repositories/job/job-db.repository.ts
+++ src/infrastructure/access/repositories/job/job-db.repository.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { CreateJobDraftInput } from '../../../../application/job/inputs';
+import {
+  CreateJobDraftInput,
+  UpdateJobDraftInput,
+} from '../../../../application/job/inputs';
 import { CreateJobDraftOutput } from '../../../../application/job/outputs';
 import { IJobDraftDbUseCase } from '../../../../application/job/usecases/job-db.interface';
 import { JobModel } from './models';
@@ -13,7 +16,13 @@ export class JobDbRepository implements IJobDraftDbUseCase {
     @InjectRepository(JobModel)
     private readonly _jobRepository: Repository<JobModel>,
   ) {}
+
   async create(data: CreateJobDraftInput): Promise<CreateJobDraftOutput> {
     return await this._jobRepository.save(data);
+  }
+
+  async update(id: string, data: UpdateJobDraftInput): Promise<boolean> {
+    const job = await this._jobRepository.findOne({ where: { id } });
+    return !!this._jobRepository.update({ id: job.id }, { ...data });
   }
 }

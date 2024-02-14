0eb52eceb9fbac882a4440b19af68174818dfe80
Anderson Vieira <asvieira.br@gmail.com>
2024-02-14 12:40:08 -0300

    feat(job): ensure DeleteJobDraftService dont delete if not found a job

--- src/application/job/services/delete-job-draft.service.ts
+++ src/application/job/services/delete-job-draft.service.ts
@@ -1,12 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { IDeleteJobDraft } from '../../../domain/usecases/job';
 import { JobDbRepository } from '../../../infrastructure/access/repositories/job';
+import { ReadJobDraftByIdService } from './read-job-draft-by-id.service';
 
 @Injectable()
 export class DeleteJobDraftService implements IDeleteJobDraft {
-  constructor(private readonly _jobDbRepository: JobDbRepository) {}
+  constructor(
+    private readonly _jobDbRepository: JobDbRepository,
+    private readonly _readJobDraftByIdService: ReadJobDraftByIdService,
+  ) {}
   async delete(id: string): Promise<boolean> {
-    return await this._jobDbRepository.delete(id);
+    const job = await this._readJobDraftByIdService.readById(id);
+
+    if (!job) {
+      throw new NotFoundException(`Job com ID ${id} não localizado`);
+    }
+
+    return await this._jobDbRepository.delete(job.id);
   }
 }

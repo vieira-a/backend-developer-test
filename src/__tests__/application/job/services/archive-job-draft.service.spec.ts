07283000edbc5a05142e9baf430f11d6baa27cb6
Anderson Vieira <asvieira.br@gmail.com>
2024-02-14 17:45:18 -0300

    test(job): ensure ArchiveJobDraftService dont archive if not found the job

--- src/__tests__/application/job/services/archive-job-draft.service.spec.ts
+++ src/__tests__/application/job/services/archive-job-draft.service.spec.ts
@@ -3,13 +3,20 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 
 import { jobMock } from '../../../../__mocks__/job';
 import { ArchiveJobDraftRequest } from '../../../../api/transports/job/requests/archive-job-draft.request';
-import { ArchiveJobDraftService } from '../../../../application/job/services';
+import {
+  ArchiveJobDraftService,
+  ReadJobDraftByIdService,
+} from '../../../../application/job/services';
 import { JobDbRepository } from '../../../../infrastructure/access/repositories/job';
 import { JobModel } from '../../../../infrastructure/access/repositories/job/models';
+
+const archiveRequest = new ArchiveJobDraftRequest();
+const archiveStatus = archiveRequest.setArchiveStatus();
 
 describe('ArchiveJobDraftService', () => {
   let service: ArchiveJobDraftService;
   let repository: JobDbRepository;
+  let readJobDraftByIdService: ReadJobDraftByIdService;
 
   beforeAll(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
@@ -20,11 +27,19 @@ describe('ArchiveJobDraftService', () => {
           provide: getRepositoryToken(JobModel),
           useValue: { archive: jest.fn() },
         },
+        ReadJobDraftByIdService,
+        {
+          provide: getRepositoryToken(JobModel),
+          useValue: { readById: jest.fn() },
+        },
       ],
     }).compile();
 
     service = moduleRef.get<ArchiveJobDraftService>(ArchiveJobDraftService);
     repository = moduleRef.get<JobDbRepository>(JobDbRepository);
+    readJobDraftByIdService = moduleRef.get<ReadJobDraftByIdService>(
+      ReadJobDraftByIdService,
+    );
   });
 
   afterEach(() => {
@@ -32,13 +47,18 @@ describe('ArchiveJobDraftService', () => {
   });
 
   it('should archive a job on success', async () => {
-    const archiveRequest = new ArchiveJobDraftRequest();
-    const archiveStatus = archiveRequest.setArchiveStatus();
-
     jest.spyOn(repository, 'archive').mockResolvedValue(true);
     jest.spyOn(service, 'archive').mockResolvedValue(true);
 
     const result = await service.archive(jobMock.id, archiveStatus);
     expect(result).toBe(true);
+  });
+
+  it('should return false if not found job by id', async () => {
+    jest.spyOn(readJobDraftByIdService, 'readById').mockResolvedValue(null);
+    jest.spyOn(service, 'archive').mockResolvedValue(false);
+
+    const result = await service.archive(jobMock.id, archiveStatus);
+    expect(result).toBe(false);
   });
 });

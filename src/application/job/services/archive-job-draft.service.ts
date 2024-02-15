67f497ad421a22782e223c2fb14f53d142f4b572
Anderson Vieira <asvieira.br@gmail.com>
2024-02-14 23:05:19 -0300

    feat(job): ensure ArchiveJobDraftService archives only published jobs

--- src/application/job/services/archive-job-draft.service.ts
+++ src/application/job/services/archive-job-draft.service.ts
@@ -1,14 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 
 import { ArchiveJob } from '../../../domain/entities/job/archive-job';
 import { IArchiveJobDraft } from '../../../domain/usecases/job';
 import { JobDbRepository } from '../../../infrastructure/access/repositories/job';
+import { ReadJobDraftByIdService } from './read-job-draft-by-id.service';
 
 @Injectable()
 export class ArchiveJobDraftService implements IArchiveJobDraft {
-  constructor(private readonly _jobDbRepository: JobDbRepository) {}
+  constructor(
+    private readonly _jobDbRepository: JobDbRepository,
+    private readonly _readJobDraftByIdService: ReadJobDraftByIdService,
+  ) {}
 
   async archive(id: string, archiveStatus: ArchiveJob): Promise<boolean> {
-    return await this._jobDbRepository.archive(id, archiveStatus);
+    const job = await this._readJobDraftByIdService.readById(id);
+
+    if (!job) {
+      throw new NotFoundException(`Job com ID ${id} não localizado`);
+    }
+
+    if (job.status !== 'published') {
+      throw new BadRequestException(
+        'Apenas postagens publicadas podem ser arquivadas',
+      );
+    }
+    return await this._jobDbRepository.archive(job.id, archiveStatus);
   }
 }

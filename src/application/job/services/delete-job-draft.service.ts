a51b001cfe850e939ee56b34cf9888ea41b37ba8
Anderson Vieira <asvieira.br@gmail.com>
2024-02-16 00:17:06 -0300

    refactor(job): add ReadJobByIdService as archive, delete and update services dependency

--- src/application/job/services/delete-job-draft.service.ts
+++ src/application/job/services/delete-job-draft.service.ts
@@ -2,16 +2,16 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { IDeleteJobDraft } from '../../../domain/usecases/job';
 import { JobDbRepository } from '../../../infrastructure/access/repositories/job';
-import { ReadJobDraftByIdService } from './read-job-draft-by-id.service';
+import { ReadJobByIdService } from './read-job-by-id.service';
 
 @Injectable()
 export class DeleteJobDraftService implements IDeleteJobDraft {
   constructor(
     private readonly _jobDbRepository: JobDbRepository,
-    private readonly _readJobDraftByIdService: ReadJobDraftByIdService,
+    private readonly _readJobByIdService: ReadJobByIdService,
   ) {}
   async delete(id: string): Promise<boolean> {
-    const job = await this._readJobDraftByIdService.readById(id);
+    const job = await this._readJobByIdService.readById(id);
 
     if (!job) {
       throw new NotFoundException(`Job com ID ${id} não localizado`);

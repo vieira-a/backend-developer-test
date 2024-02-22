5c3c76696ce08cbcc57e0b6230140761540c33e5
Anderson Vieira <asvieira.br@gmail.com>
2024-02-22 17:23:16 -0300

    test(feed): ensure FeedJobsService returns a job feed by redis cache

---
+++ src/__tests__/application/job/services/feed-jobs.service.spec.ts
@@ -0,0 +1,45 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import { feedMock } from '../../../../__mocks__/job';
+import { FeedJobsService } from '../../../../application/job/services';
+import { JobDbModel } from '../../../../infrastructure/access/repositories/job/models';
+import { S3Service } from '../../../../infrastructure/aws/s3/s3.service';
+import { RedisCacheRepository } from '../../../../infrastructure/cache/redis';
+
+describe('ReadJobByIdService', () => {
+  let service: FeedJobsService;
+  let repository: RedisCacheRepository;
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        FeedJobsService,
+        RedisCacheRepository,
+        {
+          provide: getRepositoryToken(JobDbModel),
+          useValue: { feed: jest.fn() },
+        },
+        S3Service,
+      ],
+    }).compile();
+
+    service = moduleRef.get<FeedJobsService>(FeedJobsService);
+    repository = moduleRef.get<RedisCacheRepository>(RedisCacheRepository);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be define dependencies', () => {
+    expect(service).toBeDefined();
+    expect(repository).toBeDefined();
+  });
+
+  it('should read a job feed from redis cache', async () => {
+    jest.spyOn(repository, 'feed').mockResolvedValue(feedMock);
+    const result = await service.feed();
+    expect(result).toEqual(feedMock);
+  });
+});

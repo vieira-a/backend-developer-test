22c21a62f845a5ea7b1fa53eb472acfe8f341535
Anderson Vieira <asvieira.br@gmail.com>
2024-02-22 23:49:21 -0300

    feat(docs): config swagger and global api prefix

--- src/api/main.ts
+++ src/api/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './transports/exceptions/global-exception.filter';
@@ -20,6 +21,16 @@ async function bootstrap() {
     }),
   );
 
+  app.setGlobalPrefix('api/v1');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('JPMA')
+    .setDescription('A NodeJS API for a Job Posting Management Application.')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/v1/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
